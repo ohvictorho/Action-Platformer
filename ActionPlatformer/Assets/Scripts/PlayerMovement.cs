a8f73bf6280e9f4934d3ce793091844d1bea9dcf
Victor Ho <victor.sdho@gmail.com>
2017-08-28 16:38:46 -0700

    Initial Commit

---
+++ ActionPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 10.0F;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float Translation = Input.GetAxis ("Vertical") * speed;
+		float straffe = Input.GetAxis("Horizontal") * speed;
+		Translation *= Time.deltaTime;
+		straffe *= Time.deltaTime;
+
+		transform.Translate (straffe, 0, Translation);
+
+		if (Input.GetKeyDown("escape"))
+			Cursor.lockState = CursorLockMode.None;
+	}
+}
